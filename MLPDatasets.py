7a7431d38467f23d60962b2db3d0dd6515e30224
Tim Kluser <tkluser@student.ethz.ch>
2024-01-10 17:10:38 +0100

    consecutive img dataset

--- MLPDatasets.py
+++ MLPDatasets.py
@@ -10,6 +10,8 @@ import glob
 import torchvision.transforms as T
 import torchvision.transforms.functional as F
 from classifyMotionGray import ClassifyMotionGray
+import random
+from sklearn.model_selection import train_test_split
 
 MEAN = 0.5
 STD = 0.5
@@ -82,3 +84,95 @@ class MLPDatasetValidated(Dataset): # from validation of SBU4
             ]
         )
         return trsf(img)
+    
+
+class MLPDatasetThreeConsecutive(Dataset):
+    ROOT_DIR = '/data/shared/herons/TinaDubach_data'
+
+    imsize = (2448-100, 3264) # h x w
+    def __init__(self, set="train", cameras = ["GBU3"],  resize_to = (216, 324), transform=None):           
+        self.set = set
+        self.imsize = resize_to
+
+        allFeatures = []
+        for camera in cameras:
+            allFeatures.extend(self.generateFeaturesBalanced(camera))
+
+        trainSet, testSet = train_test_split(allFeatures, test_size=0.2, random_state=1)
+
+        trainSet, valSet = train_test_split(trainSet, test_size=0.25, random_state=1)
+
+        if set == "test":
+            self.imagePaths = testSet
+        elif set == "val":
+            self.imagePaths = valSet
+        elif set == "train":
+            self.imagePaths = trainSet
+
+
+    def __len__(self):
+        return len(self.imagePaths)
+
+    def __getitem__(self, idx):
+        
+        imgs, lbl = self.imagePaths[idx]
+        try:
+            prevImg, img, nextImg = imgs
+            for i in [prevImg, img, nextImg]:
+                with open(f'/data/shared/herons/TinaDubach_data/{i[5:9]}/{i}.JPG', "rb") as f:
+                    i = Image.open(f).convert("RGB")
+                    i = self.transform(i)
+                    
+            return (prevImg, img, nextImg), self.lbl[idx], idx
+        except OSError:
+            print(f"Error occured loading the image: {img}")
+            zero = torch.zeros((3, self.imsize[0], self.imsize[1]))
+            return (
+                [zero]*3,
+                0,
+                idx
+            )
+    
+    def transform(self, img):
+        trsf = T.Compose([
+                T.ToTensor(),
+                lambda im : F.crop(im, top=0, left=0, height=2448-190, width=3264),
+                T.Resize([self.imsize[0], self.imsize[1]], antialias=True),
+                T.Normalize(mean=(MEAN, MEAN, MEAN), std=(STD, STD, STD)),
+            ]
+        )
+        return trsf(img)
+    
+    ### HELPER FUNCTIONS ###
+
+    def generateFeaturesBalanced(self, camera : str):
+        try:
+            df = pd.read_csv(f"/data/tim/heronWorkspace/MotionGrayClassification/classifiedMotionGray{camera}.csv")
+        except:
+            return []
+        
+        df = df[(df["grayscale"] == False) & (df["badImage"] == False)].sort_values(by=['ImagePath'])
+
+        sortedPaths = df["ImagePath"].tolist()
+        motions = df["motion"].astype(int).tolist()
+        features = []
+
+        for i, row in enumerate(sortedPaths):
+            _, _, nrCurr = row.split("_")
+            if i > 0 and i < len(sortedPaths)-1:
+                _, _, nrPrev = sortedPaths[i-1].split("_")
+                _, _, nrNext = sortedPaths[i+1].split("_")
+                if int(nrPrev) + 1 == int(nrCurr) and int(nrNext)-1 == int(nrCurr):
+                    features.append([(sortedPaths[i-1], row, sortedPaths[i+1]), motions[i]])
+        
+        featuresNormal = [feature for feature in features if feature[1] == 0]
+        featuresMotion = [feature for feature in features if feature[1] == 1]
+        minLen = min(len(featuresNormal), len(featuresMotion))
+
+        random.seed(1)
+        featuresNormal = random.sample(featuresNormal, minLen)
+        featuresMotion = random.sample(featuresMotion, minLen)
+
+        features = featuresNormal + featuresMotion
+        
+        return features
